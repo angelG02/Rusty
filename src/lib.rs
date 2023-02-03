07b1009ac1a13d4aed004ee094447ea3f66ebbf4
a40 <210798@buas.nl>
2023-02-04 00:22:59 +0100

    📈Multi-threaded clearing of the screen and depth buffers

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 //use glam::{Vec2, Vec3Swizzles};
 
+use std::{
+    cell::UnsafeCell,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
 pub mod camera;
 pub mod geometry;
 pub mod mesh;
@@ -16,3 +22,64 @@ pub use {
     transform::{Transform, TransformInitialParams},
     utils::*,
 };
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let builder = thread::Builder::new();
+        let thread = builder
+            .spawn(move || loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                //println!("Worker {id} got a job; executing.");
+                job();
+            })
+            .expect("Could not spawn new thread!");
+
+        Worker { id, thread }
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use glam::{Vec2, Vec4};
 use minifb::{Key, MouseButton, Window, WindowOptions};
+use std::cell::UnsafeCell;
 use std::path::Path;
 
 use rusterizer::*;
 
+const ROW_CHUNK_SIZE: usize = (WIDTH * HEIGHT) / 16;
+const GRID_SIZE: usize = (WIDTH * HEIGHT) / 16;
+
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-    let mut depth_buffer: Vec<f32> = vec![f32::INFINITY; WIDTH * HEIGHT];
+    let mut buffer: UnsafeCell<Vec<u32>> = vec![0; WIDTH * HEIGHT].into();
+    let mut depth_buffer: UnsafeCell<Vec<f32>> = vec![f32::INFINITY; WIDTH * HEIGHT].into();
 
     let aspect_ratio = WIDTH as f32 / HEIGHT as f32;
 
@@ -18,12 +22,49 @@ fn main() {
         ..Default::default()
     };
 
-    let texture = std::sync::Arc::new(Texture::load(std::path::Path::new("resources/models/SciFiHelmet/SciFiHelmet_BaseColor.png")));
+    let screen_buffer_chuncks: UnsafeCell<Vec<&mut [u32]>> = vec![].into();
+    let depth_buffer_chuncks: UnsafeCell<Vec<&mut [f32]>> = vec![].into();
+
+    unsafe {
+        for col in (*buffer.get()).chunks_mut(WIDTH * ROW_CHUNK_SIZE) {
+            for row in col.chunks_mut(ROW_CHUNK_SIZE) {
+                for chunk in row.chunks_mut(GRID_SIZE) {
+                    (*screen_buffer_chuncks.get()).push(chunk);
+                }
+            }
+        }
+
+        for col in (*depth_buffer.get()).chunks_mut(WIDTH * ROW_CHUNK_SIZE) {
+            for row in col.chunks_mut(ROW_CHUNK_SIZE) {
+                for chunk in row.chunks_mut(GRID_SIZE) {
+                    (*depth_buffer_chuncks.get()).push(chunk);
+                }
+            }
+        }
+    }
+
+    let thread_pool = ThreadPool::new(32);
+
+    let texture = std::sync::Arc::new(Texture::load(std::path::Path::new(
+        "resources/models/SciFiHelmet/SciFiHelmet_BaseColor.png",
+    )));
     let mut helmet: Model = Model::new(Path::new("resources/models/SciFiHelmet/SciFiHelmet.gltf"));
     helmet.meshes[0].add_texture(texture);
 
+    // let helmet1: Model = Model::new(Path::new("resources/models/SciFiHelmet/SciFiHelmet.gltf"));
+    // let helmet2: Model = Model::new(Path::new("resources/models/SciFiHelmet/SciFiHelmet.gltf"));
+    // let helmet3: Model = Model::new(Path::new("resources/models/SciFiHelmet/SciFiHelmet.gltf"));
+    // let helmet4: Model = Model::new(Path::new("resources/models/SciFiHelmet/SciFiHelmet.gltf"));
+
+    // let mut helmets: Vec<Model> = vec![];
+    // helmets.push(helmet.clone());
+    // helmets.push(helmet1.clone());
+    // helmets.push(helmet2.clone());
+    // helmets.push(helmet3.clone());
+    // helmets.push(helmet4.clone());
+
     let win_opts = WindowOptions {
-        resize: true,
+        resize: false,
         ..Default::default()
     };
 
@@ -39,19 +80,35 @@ fn main() {
     let mut rot = std::f32::consts::FRAC_PI_4;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        // Clear screen
-        let clear_color = Vec4::new(204.0, 255.0, 255.0, 0.0);
-        clear_screen(&mut buffer, clear_color);
-        clear_buffer(&mut depth_buffer, f32::INFINITY);
-
         // Calculate frame time (delta time)
         let new_time = std::time::Instant::now();
         let frame_time = (new_time - current_time).as_millis();
         let delta_time = frame_time as f32 / 1000.0;
         current_time = new_time;
 
-        println!("Frame time: {frame_time}ms");
+        //println!("Frame time: {frame_time}ms");
 
+        let raster_time = std::time::Instant::now();
+
+        // Clear screen and depth buffer
+        let clear_color = Vec4::new(204.0, 255.0, 255.0, 0.0);
+
+        unsafe {
+            // Multi-threaded clearing of the screen and depth buffers xddd
+            let chunks = &mut *screen_buffer_chuncks.get();
+            for chunk in chunks {
+                thread_pool.execute(move || {
+                    clear_screen(chunk, clear_color);
+                });
+            }
+
+            let chunks = &mut *depth_buffer_chuncks.get();
+            for chunk in chunks {
+                thread_pool.execute(move || {
+                    clear_buffer(&mut *chunk, f32::INFINITY);
+                });
+            }
+        }
         // Update
         camera.update(&window, delta_time);
 
@@ -61,14 +118,19 @@ fn main() {
 
         // Draw shapes
         helmet.draw(
-            &mut buffer,
-            &mut depth_buffer,
+            buffer.get_mut(),
+            depth_buffer.get_mut(),
             &mvp,
             Vec2 {
                 x: WIDTH as f32,
                 y: HEIGHT as f32,
             },
         );
+
+        // Render-only time
+        let raster_time = raster_time.elapsed().as_millis();
+        println!("Render time: {raster_time}ms");
+
         if window.get_mouse_down(MouseButton::Left) {
             rot += 1.0 * delta_time;
         } else if window.get_mouse_down(MouseButton::Right) {
@@ -76,6 +138,10 @@ fn main() {
         }
 
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+        unsafe {
+            window
+                .update_with_buffer(&*buffer.get(), WIDTH, HEIGHT)
+                .unwrap();
+        }
     }
 }
